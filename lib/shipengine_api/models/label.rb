9e10cea00112710313c67849e4d435f251fd8fa3
Sara G <platinummix@yahoo.com>
2019-06-14 16:08:54 -0500

    batch_id issue

--- lib/shipengine_api/models/label.rb
+++ lib/shipengine_api/models/label.rb
@@ -1,7 +1,7 @@
 =begin
 #ShipEngine API
 
-#ShipEngine™ is the world's best multi-carrier shipping API, with more than 20 supported shipping carriers.  You can easily get and compare shipping rates, print labels, track your shipments, and validate addresses anywhere in the world.  ### Worldwide Major Carrier Support Access the top global carriers in minutes—instead of weeks. Simply [connect your existing carrier accounts](https://docs.shipengine.com/docs/setup-a-carrier) in the API dashboard, and then [begin creating labels](https://docs.shipengine.com/docs/use-a-specific-service).  ### Real-Time Tracking [Track packages](https://docs.shipengine.com/docs/track-a-package) across any of our 20+ supported carrier accounts and create tracking events to keep your customers up-to-date. Easily integrate real-time tracking information for shipments into your app, email, or SMS.  ### Get Rates Across Carriers Make sure you ship as cost-effectively as possible by [quickly comparing rates](https://docs.shipengine.com/docs/get-shipping-rates) using the ShipEngine Rates API. As long as you have the carrier connected to your account, you'll be able to see and compare different rates and services.  ### Global Address Validation No matter your shipping volume, failed deliveries and address change surcharges cut into your bottom line and damage perception with customers. Our address validation services ensure your packages make it to the right place the first time. [Learn how to leverage our address validation services here.](https://docs.shipengine.com/docs/address-validation)  ShipEngine supports address validation for over 180 countries, including the United States, Canada, Great Britain, Australia, Germany, France, Norway, Spain, Sweden, Israel, Italy, and more. 
+#ShipEngine™ is the world's best multi-carrier shipping API, with more than 20 supported shipping carriers.  You can easily get and compare shipping rates, print labels, track your shipments, and validate addresses anywhere in the world.  ### Worldwide Major Carrier Support Access the top global carriers in minutes—instead of weeks. Simply [connect your existing carrier accounts](https://docs.shipengine.com/docs/setup-a-carrier) in the API dashboard, and then [begin creating labels](https://docs.shipengine.com/docs/use-a-specific-service).  ### Real-Time Tracking [Track packages](https://docs.shipengine.com/docs/track-a-package) across any of our 20+ supported carrier accounts and create tracking events to keep your customers up-to-date. Easily integrate real-time tracking information for shipments into your app, email, or SMS.  ### Get Rates Across Carriers Make sure you ship as cost-effectively as possible by [quickly comparing rates](https://docs.shipengine.com/docs/get-shipping-rates) using the ShipEngine Rates API. As long as you have the carrier connected to your account, you'll be able to see and compare different rates and services.  ### Global Address Validation No matter your shipping volume, failed deliveries and address change surcharges cut into your bottom line and damage perception with customers. Our address validation services ensure your packages make it to the right place the first time. [Learn how to leverage our address validation services here.](https://docs.shipengine.com/docs/address-validation)  ShipEngine supports address validation for over 180 countries, including the United States, Canada, Great Britain, Australia, Germany, France, Norway, Spain, Sweden, Israel, Italy, and more.
 
 OpenAPI spec version: 1.0.0
 Contact: sales@shipengine.com
@@ -271,21 +271,21 @@ module ShipEngineApi
         invalid_properties.push('invalid value for "is_international", is_international cannot be nil.')
       end
 
-      if @batch_id.nil?
-        invalid_properties.push('invalid value for "batch_id", batch_id cannot be nil.')
-      end
+      # if @batch_id.nil?
+      #   invalid_properties.push('invalid value for "batch_id", batch_id cannot be nil.')
+      # end
 
-      if @batch_id.to_s.length > 25
-        invalid_properties.push('invalid value for "batch_id", the character length must be smaller than or equal to 25.')
-      end
+      # if @batch_id.to_s.length > 25
+      #   invalid_properties.push('invalid value for "batch_id", the character length must be smaller than or equal to 25.')
+      # end
+      #
+      # if @batch_id.to_s.length < 1
+      #   invalid_properties.push('invalid value for "batch_id", the character length must be great than or equal to 1.')
+      # end
 
-      if @batch_id.to_s.length < 1
-        invalid_properties.push('invalid value for "batch_id", the character length must be great than or equal to 1.')
-      end
-
-      if @batch_id !~ Regexp.new(/^se(-[a-z0-9]+)+$/)
-        invalid_properties.push('invalid value for "batch_id", must conform to the pattern /^se(-[a-z0-9]+)+$/.')
-      end
+      # if @batch_id !~ Regexp.new(/^se(-[a-z0-9]+)+$/)
+      #   invalid_properties.push('invalid value for "batch_id", must conform to the pattern /^se(-[a-z0-9]+)+$/.')
+      # end
 
       if @carrier_id.nil?
         invalid_properties.push('invalid value for "carrier_id", carrier_id cannot be nil.')
